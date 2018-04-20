bc7a8bdde4520cc918ef1d47e735de9e7e594d36
Stepan <38568823+TrofimovProg@users.noreply.github.com>
2018-04-20 23:28:18 +0300

    Import code

---
+++ ufodestroyer/model/entities/Enemy.java
@@ -0,0 +1,25 @@
+package ru.myitschool.ufodestroyer.model.entities;
+
+import android.graphics.PointF;
+
+import ru.myitschool.ufodestroyer.model.math.Vector2F;
+
+/**
+ * Класс врага
+ */
+public class Enemy extends MovingGameObject {
+    private static final float TIME_BETWEEN_DIRECTION_CHANGES = 1.3f;
+    private float timeSinceLastDirectionChange = 0;
+    public int lives = 3;
+
+    @Override
+    protected void adjustVelocity(float elapsedSeconds) {
+        timeSinceLastDirectionChange += elapsedSeconds;
+        if (timeSinceLastDirectionChange > TIME_BETWEEN_DIRECTION_CHANGES) {
+            timeSinceLastDirectionChange -= TIME_BETWEEN_DIRECTION_CHANGES;
+
+            Vector2F v = getVelocity();
+            v.set(v.x, -v.y);
+        }
+    }
+}
